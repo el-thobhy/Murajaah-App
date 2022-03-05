5a07e498ac507a930d02c001fc0aec61d58ba926
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-03-05 08:50:27 +0700

    Main Activity 01

---
+++ app/src/main/java/com/elthobhy/murajaah/views/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.elthobhy.murajaah.views.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elthobhy.murajaah.R
+import com.elthobhy.murajaah.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
