5f7cac2b5ec84dc77e9fa16b798651325fce1fcb
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-03-09 14:19:10 +0700

    Menampilkan data top chart dari Realtime Database Firebase

--- app/src/main/java/com/elthobhy/murajaah/views/topcharts/TopChartsFragment.kt
+++ app/src/main/java/com/elthobhy/murajaah/views/topcharts/TopChartsFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,12 +17,36 @@ import com.elthobhy.murajaah.repository.Repository
 import com.elthobhy.murajaah.utils.hide
 import com.elthobhy.murajaah.utils.visible
 import com.elthobhy.murajaah.views.playsong.PlaySongActivity
+import com.google.firebase.database.*
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class TopChartsFragment : Fragment() {
 
     private var _binding: FragmentTopChartsBinding? = null
     private val binding get() = _binding
     private lateinit var topChartsAdapter :TopChartsAdapter
+    private lateinit var databaseTopCharts: DatabaseReference
+
+    private val eventListener = object : ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            hideLoading()
+            Log.e("cek data", "onDataChange: ${snapshot.value}", )
+            val gson = Gson().toJson(snapshot.value)
+            val type = object : TypeToken<MutableList<Song>>(){}.type
+            val song = Gson().fromJson<MutableList<Song>>(gson, type)
+
+            if(song!=null){
+                topChartsAdapter.setData(song)
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            hideLoading()
+            Log.e("check", "onCancelled: ${error.message}", )
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,13 +60,13 @@ class TopChartsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //init
         topChartsAdapter = TopChartsAdapter()
+        databaseTopCharts = FirebaseDatabase
+            .getInstance("https://murajaah-f040b-default-rtdb.asia-southeast1.firebasedatabase.app/")
+            .getReference("top_charts")
 
         showLoading()
         swipeTopChart()
-        Handler(Looper.getMainLooper()).postDelayed({
-            showTopCharts()
-        },2000)
-
+        showTopCharts()
         onClick()
 
     }
@@ -62,11 +87,8 @@ class TopChartsFragment : Fragment() {
     }
 
     private fun showTopCharts() {
-        hideLoading()
-        //GetData
-        val topChart = Repository.getFataTopChartFromAssets(context)
-        topChartsAdapter.setData(topChart as MutableList<Song>)
-
+        //GetData from firebase
+        databaseTopCharts.addValueEventListener(eventListener)
         //setup recycler View
         binding?.rvTopCharts?.adapter = topChartsAdapter
     }
